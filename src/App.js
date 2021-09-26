44c91a82be22c0b4c389eb2ffc08f52ac768a2cd
ethan.pham <ethan.pham@brayleinosplash.com>
2021-09-26 12:33:16 +0700

    testing 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Helo there!!
+          Helo there!! 123
         </p>
         <a
           className="App-link"
